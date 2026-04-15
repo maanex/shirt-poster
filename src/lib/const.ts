c7f0427fede262eef63ad3d95a3a1b280c5c2be9
Andreas <git@maanex.me>
2026-04-15 10:31:18 +0200

    oh

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -3,5 +3,5 @@
 
 export const EmojiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g
 
-export const SimpleEmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+export const SimpleEmailRegex = /[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/
 
