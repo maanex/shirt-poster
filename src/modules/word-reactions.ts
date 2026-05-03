f31fedaefcd5f8dc3c3df4e63cf98014482f2e50
Andreas <git@maanex.me>
2026-05-03 11:29:55 +0200

    more shirt

--- src/modules/word-reactions.ts
+++ src/modules/word-reactions.ts
@@ -3,6 +3,13 @@ import { defineModule } from "../lib/module"
 const mapping = {
   'sus 🤨': /(?:(?:^|\s)sus\S)|(?:\Ssus(?:\S|$))/i,
   'think!': /(?:^|\s)think(?:\s|$)/i,
+  'you\'re are': /you.{1,2}re/i,
+  'you\'re welcome': /thanks|thank you|thx|ty|tysm/i,
+  'Follow me on soundcloud!': /spotify|apple music|youtube music|deezer|tidal/i,
+  '🤑': /\d+\$/i,
+  'reference detected!!': /(?:(?:^|\s)referenc(?:\s|$))|(?:\Sreferenc(?:\S|$))/i,
+  'hello': /(?:^|\W)hi(?:\W|$)/i,
+  '🤨': /🤨|shirt/i,
 }
 
 export default defineModule({
